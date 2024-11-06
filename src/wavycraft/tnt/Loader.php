74a02aaf66703fe82b9f71841e897f39a37c091c
WavyCraftNetwork <42108413+WavyCraftNetwork@users.noreply.github.com>
2024-11-06 02:52:02 -0800

    rewrite code

--- src/wavycraft/tnt/Loader.php
+++ src/wavycraft/tnt/Loader.php
@@ -8,7 +8,19 @@ use pocketmine\plugin\PluginBase;
 
 final class Loader extends PluginBase {
 
+    private static $instance;
+
+    protected function onLoad() : void{
+        self::$instance = $this;
+    }
+
     protected function onEnable() : void{
+        $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+        $this->getServer()->getCommandMap()->register("AutoIgniteTNT", new TNTCommand());
+    }
+
+    public static function getInstance() : self{
+        return self::$instance;
     }
 }
