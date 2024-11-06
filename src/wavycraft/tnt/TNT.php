2c6b25ea21b34f4841f4a8d689d9833958b58bf3
WavyCraftNetwork <42108413+WavyCraftNetwork@users.noreply.github.com>
2024-11-06 02:53:31 -0800

    added the needed classes

---
+++ src/wavycraft/tnt/TNT.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace wavycraft\tnt;
+
+use pocketmine\block\VanillaBlocks;
+
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\enchantment\StringToEnchantmentParser;
+
+use pocketmine\player\Player;
+
+use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\TextFormat as TextColor;
+
+use pocketmine\nbt\tag\StringTag;
+
+final class TNT {
+    use SingletonTrait;
+
+    public function giveTNT(Player $player, int $amount) : Item{
+        $tnt = VanillaBlocks::TNT()->asItem();
+        $tnt->setCount($amount);
+
+        $tntName = Loader::getInstance()->getConfig()->get("tnt.name");
+        $tntLore = Loader::getInstance()->getConfig()->get("tnt.lore");
+
+        $tnt->setCustomName(TextColor::colorize($tntName));
+
+        $colorizedLore = array_map(static fn($line) => TextColor::colorize($line), (array) $tntLore);
+        $tnt->setLore($colorizedLore);
+
+        $enchantString = Loader::getInstance()->getConfig()->get("tnt.enchantment");
+        $enchantLevel = Loader::getInstance()->getConfig()->get("tnt.enchantment.level");
+
+        $enchantment = StringToEnchantmentParser::getInstance()->parse($enchantString);
+        if ($enchantment !== null) {
+            $tnt->addEnchantment(new EnchantmentInstance($enchantment, $enchantLevel));
+        }
+
+        $nbt = $tnt->getNamedTag();
+        $nbt->setTag("TNT", new StringTag("instant_tnt"));
+        $tnt->setNamedTag($nbt);
+
+        return $tnt;
+    }
+}
---
+++ src/wavycraft/tnt/TNTCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace wavycraft\tnt;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\player\Player;
+
+use pocketmine\Server;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
+
+class TNTCommand extends Command implements PluginOwned {
+
+    private $plugin;
+
+    public function __construct() {
+        parent::__construct("tnt");
+        $this->setDescription("Give auto ignite TNT to a player.");
+        $this->setUsage("/tnt <player> <amount>");
+        $this->setPermission("autoignitetnt.cmd");
+
+        $this->plugin = Loader::getInstance();
+    }
+
+    public function getOwningPlugin() : Plugin{
+        return $this->plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool{
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+
+        if (count($args) !== 2) {
+            $sender->sendMessage($this->getUsage());
+            return false;
+        }
+
+        $playerName = $args[0];
+        $amount = intval($args[1]);
+
+        if ($amount <= 0) {
+            $sender->sendMessage("Amount must be a positive integer...");
+            return false;
+        }
+
+        $targetPlayer = Server::getInstance()->getPlayerByPrefix($playerName);
+        
+        if (!$targetPlayer instanceof Player) {
+            $sender->sendMessage("The player " . $playerName . " was not found, Make sure they're online or exist");
+            return false;
+        }
+
+        $tntItem = TNT::getInstance()->giveTNT($targetPlayer, $amount);
+        $targetPlayer->getInventory()->addItem($tntItem);
+
+        $targetPlayer->sendMessage("Recieved $amount instant TNT from " . $sender->getName() . "!");
+        $sender->sendMessage("Gave $amount instant TNT to " . $targetPlayer->getName() . "!");
+        return true;
+    }
+}
